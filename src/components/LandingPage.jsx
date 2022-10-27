9359dfd9da313012084b7eaa07c9b09d5d14bbbf
Zachary Soliman <zachsoliman1@gmail.com>
2022-10-27 17:54:16 -0400

    initial commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+const LandingPage = () => {
+  return (
+    <div className="landingpage">
+      <div>
+        <h1>This is a Denstist's Website</h1>
+        <h3>Slogan or Header or something</h3>
+      </div>
+
+      <div>
+        <div>Image Placeholder</div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>LOGO</div>
+      <div>General</div>
+      <div>Cosmetic</div>
+      <div>Restorative</div>
+      <div>About</div>
+      <div>Contact</div>
+    </div>
+  );
+};
+
+export default Navbar;
