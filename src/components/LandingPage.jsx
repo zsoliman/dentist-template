c3e80b149edff39a5e541b85321ba1205f125a45
Zachary Soliman <zachsoliman1@gmail.com>
2022-10-28 16:07:34 -0400

    hello homie

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,43 +1,48 @@
+import tooth from "../media/tooth.mp4";
+import closeup from "../media/closeup.jpg";
+import toothbrush from "../media/toothbrush.jpg";
+import dentist from "../media/dentist.jpg";
+
 const LandingPage = () => {
   return (
+    
     <div>
-      <div className="landingpage">
-        <div className="landing-image">
-          <div>Image Placeholder</div>
-        </div>
-
-        <div className="landing-text">
-          <h1>This is a Denstist's Website</h1>
-          <h3>Slogan or Header or something</h3>
-        </div>
+        
+      <div className="title">
+        <h1>Your Office Name</h1>
+        <h3>"Your Slogan, because teeth are teeth"</h3>
       </div>
+      
+        <video className="video" autoPlay loop muted>
+          <source src={tooth} type="video/mp4" />
+        </video>
+
+
+
 
       <div className="landingpage">
         <div>
-          <div className="landing-text">
-            <h2>Service #1</h2>
-            <h4>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              dicta fugiat autem inventore, at aspernatur pariatur fuga quaerat
-              omnis, asperiores veritatis distinctio quo. Mollitia non ratione
-              ipsum accusantium enim eius.
-            </h4>
+          <div className="landing-image">
+            <img className="photo" alt="closeup" src={closeup} />
           </div>
         </div>
 
-        <div className="landing-image">
-          <div>Image Placeholder</div>
+        <div className="landing-text-1">
+          <h2>Service You Provide</h2>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            dicta fugiat autem inventore, at aspernatur pariatur fuga quaerat
+            omnis, asperiores veritatis distinctio quo. Mollitia non ratione
+            ipsum accusantium enim eius.
+          </h4>
         </div>
       </div>
 
       <div className="landingpage">
-        <div className="landing-image">
-          <div>Image Placeholder</div>
-        </div>
 
-        <div className="landing-text">
+        <div className="landing-text-2">
           <div>
-            <h2>Service #2</h2>
+            <h2>Other Service You Provide</h2>
             <h4>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
               dicta fugiat autem inventore, at aspernatur pariatur fuga quaerat
@@ -46,8 +51,33 @@ const LandingPage = () => {
             </h4>
           </div>
         </div>
+
+        <div className="landing-image">
+          <img className="photo" alt="toothbrush" src={toothbrush} />
+        </div>
+        
       </div>
-    </div>
+
+        <div className="landingpage">
+        <div>
+          <div className="landing-image">
+            <img className="photo" alt="dentist" src={dentist} />
+          </div>
+        </div>
+
+        <div className="landing-text-3">
+          <h2>Other Services You Provide</h2>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            dicta fugiat autem inventore, at aspernatur pariatur fuga quaerat
+            omnis, asperiores veritatis distinctio quo. Mollitia non ratione
+            ipsum accusantium enim eius.
+          </h4>
+        </div>
+      </div>
+
+      </div>
+      
   );
 };
 
